244671ee3c4d84384c1fef0e103a9e1b94254c6d
BoHLALA <bo-hlala@hotmail.com>
2025-02-25 22:47:07 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -26,10 +26,10 @@ echo $MY_SEP
 echo ''
 
 
-wget -O /var/volatile/tmp/BoHLALA_FHD.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/refs/heads/main/BoHLALA_FHD.tar.gz"
-wget -O /var/volatile/tmp/byBO-HLALA_author:skin-V_2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/refs/heads/main/byBO-HLALA_author:skin-V_2.2.tar.gz"
-wget -O /var/volatile/tmp/byBO-HLALA_author:skin-V_2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/refs/heads/main/addons1.tar.gz"
-wget -O /var/volatile/tmp/byBO-HLALA_author:skin-V_2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/refs/heads/main/addons2.tar.gz"
+wget -O /var/volatile/tmp/BoHLALA_FHD.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/raw/refs/heads/main/BoHLALA_FHD.tar.gz"
+wget -O /var/volatile/tmp/byBO-HLALA_author:skin-V_2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/raw/refs/heads/main/byBO-HLALA_authorskin-V_2.2.tar.gz"
+wget -O /var/volatile/tmp/byBO-HLALA_author:skin-V_2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/raw/refs/heads/main/addons1.tar.gz"
+wget -O /var/volatile/tmp/byBO-HLALA_author:skin-V_2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/raw/refs/heads/main/addons2.tar.gz"
 
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
 rm -rf "/usr/share/enigma2/BoHLALA_FHD"
