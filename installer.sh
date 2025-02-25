8b21dc4a0ab4880471858eab45570cfa49e88851
BoHLALA <bo-hlala@hotmail.com>
2025-02-25 23:33:06 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -32,47 +32,46 @@ wget -O /var/volatile/tmp/addons1.tar.gz --no-check-certificate "https://github.
 wget -O /var/volatile/tmp/addons2.tar.gz --no-check-certificate "https://github.com/BoHLALA/Data/raw/refs/heads/main/addons2.tar.gz"
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
 rm -rf "/usr/share/enigma2/BoHLALA_FHD"
-if [ -f $MY_TMP_FILE ]; [ -f $MY_TMP_FILE1 ]; [ -f $MY_TMP_FILE2 ]; [ -f $MY_TMP_FILE3 ]; then
-echo ''
-echo $MY_SEP
-echo 'Extracting ...'
-echo $MY_SEP
-echo ''
-tar -xzvf $MY_TMP_FILE -C /; tar -xzvf $MY_TMP_FILE1 -C /; tar -xzvf $MY_TMP_FILE2 -C /; tar -xzvf $MY_TMP_FILE3 -C /
-MY_RESULT=$?
+if [ -f "$MY_TMP_FILE" ] && [ -f "$MY_TMP_FILE1" ] && [ -f "$MY_TMP_FILE2" ] && [ -f "$MY_TMP_FILE3" ]; then
+    echo ''
+    echo $MY_SEP
+    echo 'Extracting ...'
+    echo $MY_SEP
+    echo ''
+    tar -xzvf "$MY_TMP_FILE" -C /; tar -xzvf "$MY_TMP_FILE1" -C /; tar -xzvf "$MY_TMP_FILE2" -C /; tar -xzvf "$MY_TMP_FILE3" -C /
+    MY_RESULT=$?
 
-rm -f $MY_TMP_FILE > /dev/null 2>&1
-rm -f $MY_TMP_FILE1 > /dev/null 2>&1
-rm -f $MY_TMP_FILE2 > /dev/null 2>&1
-rm -f $MY_TMP_FILE3 > /dev/null 2>&1
+    rm -f "$MY_TMP_FILE" > /dev/null 2>&1
+    rm -f "$MY_TMP_FILE1" > /dev/null 2>&1
+    rm -f "$MY_TMP_FILE2" > /dev/null 2>&1
+    rm -f "$MY_TMP_FILE3" > /dev/null 2>&1
 
-echo ''
-if [ $MY_RESULT -eq 0 ]; then
-echo "########################################################################"
-echo "#                BoHLALA v_2.2 INSTALLED SUCCESSFULLY                  #"
-echo "#                      BY TeamNitro - support on                       #"
-echo "#              https://www.tunisia-sat.com/forums/forums               #"
-echo "########################################################################"
-echo "#        Sucessfully Download Please apply from skin selection         #"
-echo "########################################################################"
-if which systemctl > /dev/null 2>&1; then
-sleep 2; systemctl restart enigma2
+    echo ''
+    if [ $MY_RESULT -eq 0 ]; then
+        echo "########################################################################"
+        echo "#                BoHLALA v_2.2 INSTALLED SUCCESSFULLY                  #"
+        echo "#                      BY TeamNitro - support on                       #"
+        echo "#              https://www.tunisia-sat.com/forums/forums               #"
+        echo "########################################################################"
+        echo "#        Sucessfully Download Please apply from skin selection         #"
+        echo "########################################################################"
+        if which systemctl > /dev/null 2>&1; then
+            sleep 2; systemctl restart enigma2
+        else
+            init 4
+            sleep 4 > /dev/null 2>&1
+            init 3
+        fi
+    else
+        echo "   >>>>   INSTALLATION FAILED !   <<<<"
+    fi
+    echo '**************************************************'
+    echo '**                   FINISHED                   **'
+    echo '**************************************************'
+    echo ''
+    exit 0
 else
-init 4
-sleep 4 > /dev/null 2>&1
-init 3
+    echo ''
+    echo "Download failed !"
+    exit 1
 fi
-else
-echo "   >>>>   INSTALLATION FAILED !   <<<<"
-fi;
-echo '**************************************************'
-echo '**                   FINISHED                   **'
-echo '**************************************************'
-echo ''
-exit 0
-else
-echo ''
-echo "Download failed !"
-exit 1
-fi
-# ----------------------------------------------------------------------------------------------------------
