45e15da1679474b830fe0b1e66433d9c92a7aea3
BO-hlala <bo-hlala@hotmail.com>
2025-03-05 11:56:56 +0300

    Update sky-Bohlala-3.sh

--- sky-Bohlala-3.sh
+++ sky-Bohlala-3.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 #
 
-# OSN Universal toppicks V1 (C) kiddac. 2024
+# Slyk Universal toppicks V5 (C) kiddac. 2019
 
 python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/scraper1.py
 
+# Slyk Universal toppicks V5 (C) kiddac. 2019
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
-if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/all_channels_data.json; then
-    python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/picker.py
-fi
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/picker1.py
+
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/*.json
 
 exit 0
