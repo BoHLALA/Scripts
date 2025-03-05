349a7e2cc527312152fd52f523554404c89b84dc
BO-hlala <bo-hlala@hotmail.com>
2025-03-05 11:11:57 +0300

    Update skyBoHLALA_FHD1.sh

--- skyBoHLALA_FHD1.sh
+++ skyBoHLALA_FHD1.sh
@@ -3,14 +3,14 @@
 
 # Slyk Universal toppicks V7 (C) kiddac. 2024
 
-python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/scraper.py
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/slyk/scraper.py
 
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
 
-if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/image_awk_scrape.json; then
-    python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/picker.py
+if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/slyk/image_awk_scrape.json; then
+    python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/slyk/picker.py
 fi
 exit 0
 
