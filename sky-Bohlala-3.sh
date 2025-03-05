a8433ff2e19498b2f482ac39016259d74a2dacc4
BO-hlala <bo-hlala@hotmail.com>
2025-03-05 11:27:42 +0300

    Update sky-Bohlala-3.sh

--- sky-Bohlala-3.sh
+++ sky-Bohlala-3.sh
@@ -3,7 +3,7 @@
 
 # OSN Universal toppicks V1 (C) kiddac. 2024
 
-python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/osn/scraper.py
+python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/scraper.py
 
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
