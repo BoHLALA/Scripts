c7bb5dcdb6ecf6f3895fb3f01b9863d8b2f15cbe
BO-hlala <bo-hlala@hotmail.com>
2025-03-12 10:54:35 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -17,7 +17,7 @@ echo $MY_SEP
 echo ''
 
 
-wget -O /var/volatile/tmp/BoHLALA_FHD.tar.gz --no-check-certificate "https://github.com/BoHLALA/Self/raw/refs/heads/main/skinData/BoHLALA_FHD.tar.gz"
+wget -O /var/volatile/tmp/BoHLALA_FHD_V2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/BoHLALA_FHD/raw/main/skins/BoHLALA_FHD_V2.2.tar.gz"
 
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
 rm -rf "/usr/share/enigma2/BoHLALA_FHD"
