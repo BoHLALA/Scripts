eafe95d41f99fe6a237931753b8b572c6832c705
BO-hlala <bo-hlala@hotmail.com>
2025-03-12 11:49:50 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,9 +5,11 @@ description= BoHlala V2.2\n Skin BoHlala ready to upgrade... Lets Fun . ^_^ !!!
 
 #########################################################
 
-#########################################################
-MY_FILE="BoHLALA_FHD_V2.2.tar.gz"
+MY_FILE="BoHLALA_app.tar.gz"
 MY_TMP_FILE="/var/volatile/tmp/"$MY_FILE
+MY_FILE1="BoHLALA_FHD_V2.2.tar.gz"
+MY_TMP_FILE1="/var/volatile/tmp/"$MY_FILE1
+MY_SEP='============================================================='
 #########################################################
 
 MY_SEP='============================================================='
@@ -16,7 +18,7 @@ echo 'Downloading '$MY_FILE' ...'
 echo $MY_SEP
 echo ''
 
-
+wget -O /var/volatile/tmp/BoHLALA_app.tar.gz --no-check-certificate "https://github.com/BoHLALA/BoHLALA_FHD/raw/main/skins/BoHLALA_app.tar.gz"
 wget -O /var/volatile/tmp/BoHLALA_FHD_V2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/BoHLALA_FHD/raw/main/skins/BoHLALA_FHD_V2.2.tar.gz"
 
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
@@ -38,8 +40,8 @@ if [ -f $MY_TMP_FILE ]; then
 	echo ''
 	if [ $MY_RESULT -eq 0 ]; then
         echo "########################################################################"
-        echo "#          TeamNitro By BoHLALA V2.3 INSTALLED SUCCESSFULLY            s#"
-        echo "#                      BY TeamNitro - support on                       #"
+        echo "#              skin BoHLALA V2.2 INSTALLED SUCCESSFULLY                #"
+        echo "#                     By BoHLALA . ^_^  support on                     #"
         echo "#              https://www.tunisia-sat.com/forums/forums               #"
         echo "########################################################################"
         echo "#        Sucessfully Download Please apply from skin selection         #"
@@ -64,6 +66,4 @@ else
 	 echo "Download failed !"
 	 exit 1
 fi
-# ----------------------------------------------------------------------------------------------------------
-
-
+# -. ^_^ ---------------------------------------------------------------------------------------------------------
