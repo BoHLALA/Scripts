a79b205324cb3942462cd496db86c15052cad25f
BO-hlala <bo-hlala@hotmail.com>
2025-03-22 15:45:10 +0300

    Update installer1.sh

--- installer1.sh
+++ installer1.sh
@@ -22,7 +22,8 @@ echo 'Downloading '$MY_FILE3' ...'
 echo $MY_SEP
 echo ''
 
-wget -O /var/volatile/tmp/interface.tar.gz --no-check-certificate "https://bohlala.github.io/Picons/refs/heads/main/interface.tar.gz"
+wget -O /var/volatile/tmp/BoHLALA_app.tar.gz --no-check-certificate "https://github.com/BoHLALA/skins/raw/main/BoHLALA_app.tar.gz"
+wget -O /var/volatile/tmp/BoHLALA_FHD_V2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/skins/raw/main/BoHLALA_FHD_V2.2.tar.gz
 
 rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
 rm -rf "/usr/share/enigma2/BoHLALA_FHD"
