0cbcb4e62e6e411a4855dcec5a9af90b760fac3c
BO-hlala <bo-hlala@hotmail.com>
2025-03-25 23:56:01 +0300

    Update skyBoHLALA_FHD2.sh

--- skyBoHLALA_FHD2.sh
+++ skyBoHLALA_FHD2.sh
@@ -10,6 +10,26 @@ echo 3 > /proc/sys/vm/drop_caches
 if test -f /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/image_awk_scrape.json; then
     python /usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD/locale/ar/slyk/picker.py
 fi
+
+
+
+
+
+
+
+echo "##################################################"
+echo "#         Developed By Bo.HLALA .. ^_^            #"
+echo "#     Updated Successfully ... Lets Fun !!!       #"
+echo "#         Thanks for using our script             #"
+echo "##################################################"
+else
+echo "   >>>>   INSTALLATION FAILED !   <<<<"
+fi;
+echo '**************************************************'
+echo '**                   FINISHED                   **'
+echo '**************************************************'
+
 exit 0
+
 
 
